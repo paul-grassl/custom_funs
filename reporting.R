7799bf92199a5ba8771b36ebbcba1e103a39ccd9
Paul Grassl <paulgrassl@Pauls-MacBook-Pro.local>
2020-05-14 17:32:43 +0200

    created report_brm function

---
+++ reporting.R
@@ -0,0 +1,17 @@
+library(glue)
+`%>%` <- magrittr::`%>%`
+
+### --- for brms estimates --- ###
+
+report_brm <- function(model, variable) {
+  effects <- brms::fixef(model) %>%
+  as.data.frame()
+  result <- glue(
+    "$\\beta$ = {papaja::printnum(effects[variable, 'Estimate'])}",
+    " ({papaja::printnum(effects[variable, 'Est.Error'])}),",
+    " CrI 95% [{papaja::printnum(effects[variable, 'Q2.5'])}, {papaja::printnum(effects[variable, 'Q97.5'])}],",
+    " OR = {papaja::printnum(exp(effects[variable, 'Estimate']))}"
+  )
+
+  return(result)
+}
