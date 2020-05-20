417beaeea869719e402ca9f1b6df107985186b93
Paul Grassl <paulgrassl@Pauls-MacBook-Pro.local>
2020-05-20 10:23:30 +0200

    made odds ratio (OR) reporting optional

--- reporting.R
+++ reporting.R
@@ -3,7 +3,7 @@ library(glue)
 
 ### --- for brms estimates --- ###
 
-report_brm <- function(x, variable) {
+report_brm <- function(x, variable, OR = FALSE) {
   # check whether it's a model or a results object
   if (class(x) != 'data.frame') {
     effects <- brms::fixef(x) %>%
@@ -15,9 +15,10 @@ report_brm <- function(x, variable) {
   result <- glue(
     "$\\beta$ = {papaja::printnum(effects[variable, 'Estimate'])}",
     " ({papaja::printnum(effects[variable, 'Est.Error'])}),",
-    " CrI 95% [{papaja::printnum(effects[variable, 'Q2.5'])}, {papaja::printnum(effects[variable, 'Q97.5'])}],",
-    " OR = {papaja::printnum(exp(effects[variable, 'Estimate']))}"
+    " CrI 95% [{papaja::printnum(effects[variable, 'Q2.5'])}, {papaja::printnum(effects[variable, 'Q97.5'])}]"
   )
+
+  if (OR) result <- glue(result, ", OR = {papaja::printnum(exp(effects[variable, 'Estimate']))}")
 
   return(result)
 }
